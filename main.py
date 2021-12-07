38e7b4f6786e49463eaa723795299bab03cee676
Ivan Pleshkov <iipleshkov@miem.hse.ru>
2021-12-08 00:38:34 +0300

    test commit

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Hello world")
+    bot.reply_to(message, "Hello world!")
 
 
 bot.infinity_polling()
