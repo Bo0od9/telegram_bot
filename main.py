9f2ae63025c79e198adf208df24590b7346c7060
Ivan Pleshkov <iipleshkov@miem.hse.ru>
2021-12-08 00:38:06 +0300

    test commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import telebot
+from telebot import types
+from config import BOT_TOKEN
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Hello world")
+
+
+bot.infinity_polling()
