38b7d6ebd74f197760bda901f3ca80e1d53f4a49
Ivan Pleshkov <iipleshkov@miem.hse.ru>
2021-12-15 15:18:56 +0300

    test commit

--- main.py
+++ main.py
@@ -6,8 +6,14 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 
 @bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Hello world!")
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    btn1 = types.KeyboardButton('Зашифровать сообщение')
+    btn2 = types.KeyboardButton('Расшифровать сообщение')
+    btn3 = types.KeyboardButton('О боте')
+    markup.add(btn1, btn2, btn3)
+    send_mess = f"<b>Привет {message.from_user.first_name}</b>!\nЧем могу помочь?"
+    bot.send_message(message.chat.id, send_mess, parse_mode='html', reply_markup=markup)
 
 
 bot.infinity_polling()
