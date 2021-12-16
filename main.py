0e2deae357c1ceaf9404c1310c1b6a66c17728e1
Ivan Pleshkov <iipleshkov@miem.hse.ru>
2021-12-16 11:49:37 +0300

    test commit

--- main.py
+++ main.py
@@ -16,4 +16,32 @@ def start(message):
     bot.send_message(message.chat.id, send_mess, parse_mode='html', reply_markup=markup)
 
 
-bot.infinity_polling()
+@bot.message_handler(content_types=['text'])
+def mess(message):
+    get_messsage_bot = message.text.strip().lower()
+
+    if get_messsage_bot == "назад":
+        start(message)
+    elif get_messsage_bot == "зашифровать сообщение":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+        btn1 = types.KeyboardButton('Шифр Виженера')
+        btn2 = types.KeyboardButton('Шифр Цезаря')
+        btn3 = types.KeyboardButton('Назад')
+        markup.add(btn1, btn2, btn3)
+        final_message = "Выберите метод шифрования"
+        bot.send_message(message.chat.id, final_message, parse_mode='html', reply_markup=markup)
+    elif get_messsage_bot == 'шифр виженера':
+        markup = types.ReplyKeyboardRemove(selective=False)
+        bot.send_message(message.chat.id,'Текст должен быть на латинице, возможно использование цифр и специальных символов!',parse_mode='html', reply_markup=markup)
+        bot.send_message(message.chat.id, '<b>Введите текст для шифрования:</b>', parse_mode='html',reply_markup=markup)
+        def add_text(message):
+            text = message.text
+            bot.send_message(message.chat.id, '<b>Введите секртеный ключ:</b>', parse_mode='html',
+                             reply_markup=markup)
+
+            print(text)
+        bot.register_next_step_handler(message, add_text)
+
+
+# bot.infinity_polling()
+bot.polling(none_stop=True)
